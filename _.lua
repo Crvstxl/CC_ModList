810dbab4b515af9e6d61d6e74c42eafc532f6fc0
Crvstxl <92829005+Crvstxl@users.noreply.github.com>
2021-12-22 20:09:11 +0100

    Update _.lua

--- _.lua
+++ _.lua
@@ -1,1 +1,13 @@
-2495657990
+local Whitelisted = {
+  2495657990,
+}
+
+local function GetModUser(Id)
+    local Found = {}
+    for i,v in pairs(game.Players:GetChildren()) do
+        if table.find(Whitelisted, Id) then
+            print("User Found")
+        end
+    end
+    return Found
+end
